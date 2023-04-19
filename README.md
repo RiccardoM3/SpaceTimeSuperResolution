58e8374fae90ac47464e9bfb99f2dd0a7ad9fc94
Riccardo Menon <ric.menon2000@gmail.com>
2023-04-19 21:18:21 +1000

    Add framework code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# SpaceTimeSuperResolution
+# SRSRT: Spacetime Realtime Super Resolution Transformer
+# Introduces a u-net style transformer in both the spacial and temporal domain
+# Employs a modification of the swin transformer
+
+
+
+
+TODO:
+i need to see if attention k,v dictionaries can be convolved usefully to lower resolutions
+
+# Running SRSRT:
+## Data preparation
+python SRSRT.py 
+
+## Training
+``python SRSRT.py prepare_training <training_set_path>``
+``python SRSRT.py prepare_evaluation <evaluation_set_path>``
+``python SRSRT.py train <model_name> <training_set_path>``
+``python SRSRT.py evaluate <model_name> <evaluation_set_path>``
---
+++ SRSRT.py
@@ -0,0 +1,55 @@
+import signal
+import sys
+import os
+
+class SRSRT:
+    def __init__(self):
+        pass
+
+    def perform_command_line_args(self):
+        if len(sys.argv) == 1:
+            print("Please provide command line arguments")
+            sys.exit(0)
+
+        if sys.argv[1] == "prepare_training":
+            if len(sys.argv) == 3 and os.path.isdir(sys.argv[2]):
+                self.prepare_training(sys.argv[2])
+                return
+        elif sys.argv[1] == "prepare_evaluation":
+            if len(sys.argv) == 3 and os.path.isdir(sys.argv[2]):
+                self.prepare_evaluation(sys.argv[2])
+                return
+        elif sys.argv[1] == "train":
+            if len(sys.argv) == 4 and os.path.isdir(sys.argv[3]):
+                self.train(sys.argv[2], sys.argv[3])
+                return
+        elif sys.argv[1] == "evaluate":
+            if len(sys.argv) == 4 and os.path.isdir(sys.argv[3]):
+                self.evaluate(sys.argv[2], sys.argv[3])
+                return
+
+        print("Please provide correct command line arguments and ensure your path is valid")
+        sys.exit(0)
+        
+    def prepare_training(self, training_path):
+        pass
+
+    def prepare_evaluation(self, evaluation_path):
+        pass
+
+    def train(self, model_name, training_path):
+        stop_training = False
+        def signal_handler(_, __):
+            print("Will pause training when ready")
+            stop_training = True
+        signal.signal(signal.SIGINT, signal_handler)
+
+        while not stop_training:
+            print("train")
+        
+    def evaluate(self, model_name, evaluation_path):
+        pass
+
+if __name__ == "__main__":
+    program = SRSRT()
+    program.perform_command_line_args()
