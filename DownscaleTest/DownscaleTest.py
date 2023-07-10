33ab066ad21a36eec3f7d64c7504dfae412934bf
Riccardo Menon <ric.menon2000@gmail.com>
2023-07-10 13:22:55 +1000

    Added a downscaling test

---
+++ DownscaleTest/DownscaleTest.py
@@ -0,0 +1,33 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv2.imread('./test4.png')
+img = img[:, :, [2, 1, 0]] # swap the channels of the image
+scale = 4
+
+interpolations = [
+    cv2.INTER_NEAREST,
+    cv2.INTER_LINEAR,
+    cv2.INTER_CUBIC,
+    cv2.INTER_AREA,
+    cv2.INTER_LANCZOS4,
+    cv2.INTER_LINEAR_EXACT
+]
+
+fig, axs = plt.subplots(3, 3, figsize=(18,16), sharex=True, sharey=True)
+fig.subplots_adjust(wspace=0, hspace=0)
+
+axs[0][0].axis('off')
+axs[0][1].axis('off')
+axs[0][2].axis('off')
+axs[0][1].imshow(img)
+
+for i in range(len(interpolations)):
+    scaled_img = cv2.resize(img, None, fx=1./scale, fy=1./scale, interpolation=interpolations[i])
+    scaled_img = np.repeat(np.repeat(scaled_img, scale, axis=0), scale, axis=1)
+    axs[1+i//3][i%3].imshow(scaled_img)
+    axs[1+i//3][i%3].axis('off')
+     
+
+plt.show()
